478759ebb02440b4524007b65ae23db1fafbfbc3
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-04-25 02:44:45 +0530

    Update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
