da26d432fc304cd2b8bf65281aed14006118b0d4
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-04-25 22:49:22 +0530

    User module init

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
-import { UserService } from './services/user.service';
+import { User, UserSchema } from './user.schema';
+import { UserService } from './user.service';
 
 @Module({
   imports: [
