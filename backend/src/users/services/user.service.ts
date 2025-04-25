a261a354516d39a0bea61af27abe57aece097f4e
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-04-25 20:18:22 +0530

    small change

--- backend/src/users/services/user.service.ts
+++ backend/src/users/services/user.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { User, UserDocument } from '../schemas/user.schema';
-import { CreateUserDto } from '../dto/create-user.dto';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 @Injectable()
 export class UserService {
