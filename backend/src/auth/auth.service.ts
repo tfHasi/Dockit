2b5948d988aa74186dba43f22b1642012332efcd
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-04-26 06:16:07 +0530

    adjust payload to return nickname

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -32,7 +32,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { email: user.email, sub: user._id };
+    const payload = { email: user.email, sub: user._id, nickname: user.nickname,};
     return {
       access_token: this.jwtService.sign(payload),
       user: {
