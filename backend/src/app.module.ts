0e51993a6cc0b3ee72b47199be455787e48f3ace
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-04-27 14:04:07 +0530

    cleaning

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MessagesModule } from './messages/messages.module';
@@ -25,7 +23,5 @@ import { WebsocketsModule } from './websockets/websockets.module';
     MessagesModule,
     WebsocketsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
